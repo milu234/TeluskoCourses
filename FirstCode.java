999744774adc082e16281652bae028d30268579c
milu234 <milanhazra234.mh@gmail.com>
2019-06-11 11:42:21 +0530

    DataTypes and Variables

---
+++ FirstCode.java
@@ -0,0 +1,10 @@
+//"print" method is called by "out" object and System is the class in System.out.print("")
+//In public static void main(String args[]), public is the type of the access specifier static is the type of method
+//and void is the return type
+// Java Says everything should be in a class
+//set path = "c:\Program Files\Java\jdk1.8.0\bin"
+class FirstCode{
+	public static void main(String args[]){
+		System.out.println("Hello");
+	}
+}
